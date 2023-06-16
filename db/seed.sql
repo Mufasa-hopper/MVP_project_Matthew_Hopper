fabdfacc40d7a7e4e4d42987454305c67a3beb19
Mufasa-hopper <hopperisawsome@gmail.com>
2023-06-16 08:08:42 -0700

    Added more drinks to seed goodDrinks table.

--- db/seed.sql
+++ db/seed.sql
@@ -1,9 +1,15 @@
 -- Seed the table "goodDrinks" with information using "INSERT INTO" and "VALUES"
 INSERT INTO goodDrinks (drinkName, liquorBase, addOns, if_ice)
-VALUES ('Tequila Sunrise', 'Tequila', 'orange juice and grenadine syrup', true),
-       ('Old Fashioned', 'Whiskey', 'sugar cube, bitters, orange slice, and a cherry', true),
-       ('Dark and Stormy', 'Rum', 'ginger beer and lime', true),
-       ('Martini', 'Gin', 'dry vermouth, lemon twist, and olives', false);
+VALUES 
+    ('Tequila Sunrise', 'Tequila', 'orange juice, and grenadine syrup', true),
+    ('Old Fashioned', 'Whiskey', 'sugar cube, bitters, orange slice, and a cherry', true),
+    ('Dark and Stormy', 'Rum', 'ginger beer, and lime', true),
+    ('Martini', 'Gin', 'dry vermouth, lemon twist, and olives', false),
+    ('Mojito', 'Rum', 'fresh mint leaves, lime juice, simple syrup, and club soda', true),
+    ('Cosmopolitan', 'Vodka', 'cranberry juice, lime juice, and triple sec', false),
+    ('Margarita', 'Tequila', 'lime juice, triple sec, and salt rim', true),
+    ('Whiskey Sour', 'Whiskey', 'lemon juice, simple syrup, and a cherry', true),
+    ('Piña Colada', 'Rum', 'pineapple juice, coconut cream, and a pineapple wedge', true);
 
 -- Seed the table "bars" with information using "INSERT INTO" and "VALUES"
 INSERT INTO bars (barName, yearEstablished)
