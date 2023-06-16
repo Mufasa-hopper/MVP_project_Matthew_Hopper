4fcdbce017778d1d3baddaede048c9ac927463d0
Mufasa-hopper <hopperisawsome@gmail.com>
2023-06-16 12:50:20 -0700

    the start of creating routes, CRUD functionallity

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { Pool } = require('ps');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+
+
+const sql = postgres(process.env.DATABASE_URL);
+const PORT = process.env.PORT
+
+const pool = new Pool({
+    connectionString: dbString
+})
+
+app.use(express.static("public"));
+
+app.get("/goodDrinks", async (res , res) => {
+    try {
+        const result = await Pool.query("SELECT * FROM goodDrinks")
+        res.json(result.rows).status(200)
+    } catch (err) {
+        res.json(err.message).status(500)
+    }
+})
+
+// TODO: Replace 3000 with process.env.PORT
+app.listen(process.env.PORT, () => {
+  console.log(`listening on Port ${3000}`);
+});
+
+app.listen (PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+})
