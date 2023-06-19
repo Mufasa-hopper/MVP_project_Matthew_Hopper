b566b8787f51c97aacd4e8f07f209530b1df3e39
Mufasa-hopper <hopperisawsome@gmail.com>
2023-06-19 12:02:46 -0700

    additional changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const {Pool} = require('pg');
+const { Pool } = require('pg');
 const app = express();
 
 dotenv.config();
@@ -9,21 +9,21 @@ const dbString = process.env.DATABASE_URL;
 const PORT = process.env.PORT;
 
 const pool = new Pool({
-    connectionString: dbString
-})
+  connectionString: dbString,
+});
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/goodDrinks", async (req , res) => {
-    try {
-        const result = await pool.query("SELECT * FROM goodDrinks")
-        res.json(result.rows).status(200)
-    } catch (err) {
-        res.json(err.message).status(500)
-    }
-})
+app.get('/goodDrinks', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM goodDrinks');
+    res.json(result.rows).status(200);
+  } catch (err) {
+    console.error('Error executing query:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-
-app.listen (PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
