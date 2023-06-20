ce82e2fa108b17d55f2b3270838cef6847ca7cb1
Mufasa-hopper <hopperisawsome@gmail.com>
2023-06-20 09:07:16 -0700

    Major updates to server and client side

--- public/main.js
+++ public/main.js
@@ -1,1 +1,162 @@
-console.log('js file is hooked up');
+// Register a new user
+async function registerUser(username, email, password) {
+    try {
+      const response = await fetch('/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, email, password }),
+      });
+  
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error);
+      }
+  
+      const userData = await response.json();
+      const userId = userData.userId;
+      // You can store the user ID in the client-side storage or use it for subsequent requests
+      return userId;
+    } catch (error) {
+      console.error('Error registering user:', error);
+      // Handle error
+    }
+  }
+  
+  // Log in a user
+  async function loginUser(username, password) {
+    try {
+      const response = await fetch('/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+  
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error);
+      }
+  
+      const userData = await response.json();
+      const token = userData.token;
+      // You can store the session token in the client-side storage or use it for subsequent requests
+      return token;
+    } catch (error) {
+      console.error('Error logging in user:', error);
+      // Handle error
+    }
+  }
+  
+  // Add a review for a specific drink
+  async function addReview(drinkId, rating, reviewText, token) {
+    try {
+      const response = await fetch(`/drinks/${drinkId}/reviews`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`, // Include the session token in the request headers
+        },
+        body: JSON.stringify({ rating, reviewText }),
+      });
+  
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error);
+      }
+  
+      const reviewData = await response.json();
+      const reviewId = reviewData.reviewId;
+      // Handle successful review addition
+      console.log('Review added successfully. Review ID:', reviewId);
+    } catch (error) {
+      console.error('Error adding review:', error);
+      // Handle error
+    }
+  }
+  
+  // Update a review
+  async function updateReview(reviewId, rating, reviewText, token) {
+    try {
+      const response = await fetch(`/reviews/${reviewId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`, // Include the session token in the request headers
+        },
+        body: JSON.stringify({ rating, reviewText }),
+      });
+  
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error);
+      }
+  
+      // Handle successful review update
+      console.log('Review updated successfully.');
+    } catch (error) {
+      console.error('Error updating review:', error);
+      // Handle error
+    }
+  }
+  
+  // Delete a review
+  async function deleteReview(reviewId, token) {
+    try {
+      const response = await fetch(`/reviews/${reviewId}`, {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${token}`, // Include the session token in the request headers
+        },
+      });
+  
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error);
+      }
+     // Handle successful review deletion
+     console.log('Review deleted successfully.');
+    } catch (error) {
+      console.error('Error deleting review:', error);
+      // Handle error
+    }
+  }
+  
+  // Example usage
+  async function exampleUsage() {
+    try {
+      // Register a new user
+      const username = 'johnDoe';
+      const email = 'johndoe@example.com';
+      const password = 'password123';
+      const userId = await registerUser(username, email, password);
+      console.log('User registered successfully. User ID:', userId);
+  
+      // Log in the user
+      const token = await loginUser(username, password);
+      console.log('User logged in successfully. Token:', token);
+  
+      // Add a review for a drink
+      const drinkId = '12345'; // Specify the ID of the drink
+      const rating = 4;
+      const reviewText = 'Great drink!';
+      await addReview(drinkId, rating, reviewText, token);
+  
+      // Update a review
+      const reviewId = '67890'; // Specify the ID of the review
+      const updatedRating = 5;
+      const updatedReviewText = 'Excellent drink!';
+      await updateReview(reviewId, updatedRating, updatedReviewText, token);
+  
+      // Delete a review
+      const reviewIdToDelete = '12345'; // Specify the ID of the review to delete
+      await deleteReview(reviewIdToDelete, token);
+    } catch (error) {
+      console.error('Example usage error:', error);
+    }
+  }
+  
+  // Run the example usage
+  exampleUsage();
--- server.js
+++ server.js
@@ -11,10 +11,64 @@ const PORT = process.env.PORT;
 const pool = new Pool({
   connectionString: dbString,
 });
-
+app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/goodDrinks', async (req, res) => {
+app.post('/register', async (req, res) => {
+  const { username, email, password } = req.body;
+
+  // Validate input data (e.g., check if required fields are present)
+
+  try {
+    // Check if the username or email already exists in the database
+    const existingUser = await pool.query(
+      'SELECT * FROM users WHERE username = $1 OR email = $2',
+      [username, email]
+    );
+
+    if (existingUser.rows.length > 0) {
+      return res.status(400).json({ error: 'Username or email already exists' });
+    }
+
+    // Insert the new user into the database
+    const newUser = await pool.query(
+      'INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id',
+      [username, email, password]
+    );
+
+    res.status(200).json({ message: 'User registered successfully', userId: newUser.rows[0].id });
+  } catch (err) {
+    console.error('Error executing query:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Authenticate and log in a user
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  // Validate input data (e.g., check if required fields are present)
+
+  try {
+    // Check if the username and password match a user in the database
+    const user = await pool.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password]);
+
+    if (user.rows.length === 0) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    // Generate a session token or use cookies for session management
+    const token = generateSessionToken();
+
+    res.status(200).json({ message: 'User logged in successfully', token });
+  } catch (err) {
+    console.error('Error executing query:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Retrieve a list of available drinks
+app.get('/drinks', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM goodDrinks');
     res.json(result.rows).status(200);
@@ -24,6 +78,110 @@ app.get('/goodDrinks', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
+// Add a review for a specific drink
+app.post('/drinks/:drinkId/reviews', async (req, res) => {
+  const { drinkId } = req.params;
+  const { rating, reviewText } = req.body;
+
+  // Validate input data (e.g., check if required fields are present)
+
+  try {
+    // Check if the drink exists in the database
+    const drink = await pool.query('SELECT * FROM goodDrinks WHERE id = $1', [drinkId]);
+
+    if (drink.rows.length === 0) {
+      return res.status(404).json({ error: 'Drink not found' });
+    }
+
+    // Insert the review into the database
+    const newReview = await pool.query(
+      'INSERT INTO userDrinkReviews (userId, drinkId, rating, reviewText) VALUES ($1, $2, $3, $4) RETURNING reviewId',
+      [loggedInUserId, drinkId, rating, reviewText]
+    );
+
+    res.status(200).json({ message: 'Review added successfully', reviewId: newReview.rows[0].reviewId });
+  } catch (err) {
+    console.error('Error executing query:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Retrieve the drink history of a specific user
+app.get('/users/:userId/drink-history', async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const result = await pool.query(
+      'SELECT goodDrinks.*, userDrinkReviews.rating, userDrinkReviews.reviewText ' +
+      'FROM goodDrinks ' +
+      'JOIN userDrinkReviews ON goodDrinks.id = userDrinkReviews.drinkId ' +
+      'WHERE userDrinkReviews.userId = $1',
+      [userId]
+    );
+    res.json(result.rows).status(200);
+  } catch (err) {
+    console.error('Error executing query:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Retrieve the profile information of a specific user
+app.get('/users/:userId/profile', async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const result = await pool.query('SELECT * FROM users WHERE id = $1', [userId]);
+    res.json(result.rows[0]).status(200);
+  } catch (err) {
+    console.error('Error executing query:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Update a review
+app.put('/reviews/:reviewId', async (req, res) => {
+  const { reviewId } = req.params;
+  const { rating, reviewText } = req.body;
+
+  // Validate input data (e.g., check if required fields are present)
+
+  try {
+    // Check if the review exists in the database
+    const existingReview = await pool.query('SELECT * FROM userDrinkReviews WHERE reviewId = $1', [reviewId]);
+
+    if (existingReview.rows.length === 0) {
+      return res.status(404).json({ error: 'Review not found' });
+    }
+
+    // Update the review in the database
+    await pool.query(
+      'UPDATE userDrinkReviews SET rating = $1, reviewText = $2 WHERE reviewId = $3',
+      [rating, reviewText, reviewId]
+    );
+
+    res.status(200).json({ message: 'Review updated successfully' });
+  } catch (err) {
+    console.error('Error executing query:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Delete a review
+app.delete('/reviews/:reviewId', async (req, res) => {
+  const { reviewId } = req.params;
+
+  try {
+    // Check if the review exists in the database
+    const existingReview = await pool.query('SELECT * FROM userDrinkReviews WHERE reviewId = $1', [reviewId]);
+
+    if (existingReview.rows.length === 0) {
+      return res.status(404).json({ error: 'Review not found' });
+    }
+
+    // Delete the review from the database
+    await pool.query('DELETE FROM userDrinkReviews WHERE reviewId = $1', [reviewId]);
+
+    res.status(200).json({ message: 'Review deleted successfully' });
+  } catch (err) {
+    console.error('Error executing query:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
